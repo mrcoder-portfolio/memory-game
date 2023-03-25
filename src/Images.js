dd19ba88b75d4cd0aeb7bb8a29d7098bf511d7b3
Mian Mohid Naeem <113250876+mrcoder-portfolio@users.noreply.github.com>
2023-03-25 19:51:11 +0500

    Add files via upload

---
+++ src/Images.js
@@ -0,0 +1,20 @@
+import Image1 from './memes/01.jpg';
+import Image2 from './memes/02.jpg';
+import Image3 from './memes/03.jpg';
+import Image4 from './memes/04.jpg';
+import Image5 from './memes/05.jpg';
+import Image6 from './memes/06.jpg';
+import Image7 from './memes/07.jpg';
+import Image8 from './memes/08.jpg';
+import Image9 from './memes/09.jpg';
+import Image10 from './memes/10.jpg';
+import Image11 from './memes/11.jpg';
+import Image12 from './memes/12.jpg';
+import Image13 from './memes/13.jpg';
+import Image14 from './memes/14.jpg';
+import Image15 from './memes/15.jpg';
+import Image16 from './memes/16.jpg';
+import Image17 from './memes/17.jpg';
+import Image18 from './memes/18.jpg';
+
+export default [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9, Image10, Image11, Image12, Image13, Image14, Image15, Image16, Image17, Image18]
